787ef7c88a41fdc4d1b7d90768c2fb885eb61c89
Alice <alicexenawong@gmail.com>
2020-01-09 18:57:12 -0800

    Integrate theme on games pages (index and show).

--- src/views/GamesShow.vue
+++ src/views/GamesShow.vue
@@ -1,5 +1,48 @@
 <template>
   <div class="home">
+    <section class="wrapper style1">
+          <div class="container">
+            <div class="row">
+              <section class="col-6 col-12-narrower">
+                <div class="box post">
+                  <a href="#" class="image left"><img src="images/pic01.jpg" alt="" /></a>
+                  <div class="inner">
+                    <h3>{{game.title}}</h3>
+                    <p>{{game.description}}</p>
+                    <p>Release Year: {{game.release_year}}</p>
+                    <p>Cumulative Rating: {{game.cumulative_rating}}</p>
+                    <p>Genres:</p>
+                    <div v-for="genre in game.genre">
+                      {{genre.genre}}
+                    </div>
+                    <br>
+                    <p>Platforms:</p>
+                    <div v-for="platform in game.platform">
+                      {{platform.platform}}
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </section>
+        <section class="wrapper style1">
+          <div class="container">
+            <div class="row" v-for="review in game.review">
+              <section class="col-6 col-12-narrower">
+                <div class="box post">
+                  <a href="#" class="image left"><img src="images/pic01.jpg" alt="" /></a>
+                  <div class="inner">
+                    <h3>Review</h3>
+                    <p>User: {{review.user}}</p>
+                    <p>Rating: {{review.rating}}</p>
+                    <p>{{review.review}}</p>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </section>
     <h1>{{ message }}</h1>
     <h2>{{game.title}}</h2>
     <p>ID: {{game.id}}</p>
