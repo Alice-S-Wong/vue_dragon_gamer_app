3bcafeaaba76e808c14e58fc9d553304dacb216e
Alice <alicexenawong@gmail.com>
2020-01-20 17:37:23 -0800

    List item deletion only available to current user's lists. Not other users' lists.

--- src/views/ListShow.vue
+++ src/views/ListShow.vue
@@ -3,7 +3,7 @@
     <h1>{{ message }}</h1>
     <h2>{{list.user}}'s {{list.list_type}}</h2>
     <div v-for="game in list.games">
-      {{game.game}} <button v-on:click="destroyItem(game)">Remove from List</button>
+      {{game.game}} <button v-if="user.username === list.user" v-on:click="destroyItem(game)">Remove from List</button>
     </div>
     <br>
     <router-link to="/lists">Return to list index</router-link>
@@ -19,12 +19,18 @@ export default {
   data: function() {
     return {
       message: "View List",
-      list: {}
+      list: {},
+      user: {},
     };
   },
   created: function() {
     axios.get(`/api/lists/${this.$route.params.id}`).then(response => {
       this.list = response.data;
+    });
+    axios.get("/api/users").then(response => {
+      if (response.data) {
+        this.user = response.data;
+      }
     });
   },
   methods: {
